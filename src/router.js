5010d9fc37a784a389e093654ea368f84a9d427c
Charlie Gilman <charliegilmanuk@gmail.com>
2019-01-27 22:07:37 +0000

    Remove default routes/views, add shop and product

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,17 +9,26 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: { name: 'shop' }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'shop',
+      path: '/shop',
+      component: require('~/views/Shop').default,
+      meta: {
+        title: 'Shop'
+      }
+    },
+    {
+      path: '/products',
+      redirect: { name: 'shop' },
+      children: [
+        {
+          name: 'product',
+          path: ':id',
+          component: require('~/views/SingleProduct').default
+        }
+      ]
     }
   ]
 });
