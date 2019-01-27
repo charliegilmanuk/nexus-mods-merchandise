95c9335a2972810001f259fdf418ea47c219d310
Charlie Gilman <charliegilmanuk@gmail.com>
2019-01-27 22:05:27 +0000

    Add helpers mixin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import './plugins/vuetify';
-import App from './App.vue';
+import App from './App';
 import router from './router';
 import store from './store';
+import helpers from './mixins/helpers';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
+
+Vue.mixin(helpers);
 
 new Vue({
   router,
---
+++ src/mixins/helpers.js
@@ -0,0 +1,9 @@
+const helpers = {
+  methods: {
+    outOfScope() {
+      console.log('This action is out of the scope of this demo');
+    }
+  }
+};
+
+export default helpers;
