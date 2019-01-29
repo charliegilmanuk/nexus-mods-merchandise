d99c739eeeec5813f3aa771a423714a5cea68cec
Charlie Gilman <charliegilmanuk@gmail.com>
2019-01-29 23:54:25 +0000

    Replace placeholder images

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,7 @@ const products = [
     1,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-27 17:00:00',
     375,
     2000
@@ -14,7 +14,7 @@ const products = [
     2,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-01-14 17:00:00',
     4199,
     10000
@@ -23,16 +23,16 @@ const products = [
     3,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-grey-front.jpg'),
     '2019-02-10 17:00:00',
-    1566,
+    1750,
     1750
   ),
   new Product(
     4,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-27 17:00:00',
     288,
     500
@@ -41,7 +41,7 @@ const products = [
     5,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-18 17:00:00',
     1466,
     1500
@@ -50,7 +50,7 @@ const products = [
     6,
     'Nexus Mods T-Shirt',
     `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
-    'https://placehold.it/600x400',
+    require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-04-01 17:00:00',
     3780,
     15000
