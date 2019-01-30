7e14035432465f8e0d7bbe378a25fadb10edfb82
Charlie Gilman <charliegilmanuk@gmail.com>
2019-01-30 23:52:41 +0000

    Add single product basic layout/logic

--- src/views/SingleProduct.vue
+++ src/views/SingleProduct.vue
@@ -1,9 +1,65 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <v-layout wrap justify-center class="py-5">
+        <v-flex xs12 md5 lg4>
+          <v-img :src="product.image" aspect-ratio="1"></v-img>
+        </v-flex>
+        <v-flex xs12 md7 lg4 offset-lg1>
+          <h1 class="font-weight-light display-3 text-uppercase">
+            {{ product.name }}
+          </h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container fluid class="grey darken-3">
+      <v-layout wrap justify-center class="py-5">
+        <v-flex xs12 md10 lg8>
+          <v-tabs grow color="transparent" slider-color="primary">
+            <v-tab v-for="tab in tabs" :key="tab">
+              {{ tab }}
+            </v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="tabActive" class="pa-5">
+            <v-tab-item :key="Description">
+              <h1>Description</h1>
+            </v-tab-item>
+            <v-tab-item :key="Comments">
+              <h1>Comments</h1>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import products from '~/data/products';
 
-<style></style>
+export default {
+  data: () => ({
+    tabActive: null,
+    tabs: ['Description', 'Comments', 'FAQ']
+  }),
+  computed: {
+    products() {
+      return products;
+    },
+
+    productId() {
+      return this.$route.params.id;
+    },
+
+    product() {
+      return this.products.find(x => x.id === this.productId);
+    }
+  },
+  created() {
+    console.log('product mounted');
+
+    console.log('products', this.products);
+    console.log('productId', this.productId);
+  }
+};
+</script>
