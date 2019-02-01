577565d0f26a04dbdbdb5116121e28b808bb521d
Charlie Gilman <charliegilmanuk@gmail.com>
2019-02-01 23:48:17 +0000

    Add draft admin routes

--- src/router.js
+++ src/router.js
@@ -27,6 +27,26 @@ export default new Router({
       name: 'product',
       path: '/products/:id',
       component: require('~/views/SingleProduct').default
+    },
+    {
+      name: 'admin',
+      path: '/admin',
+      redirect: { name: 'admin.products' }
+    },
+    {
+      name: 'admin.products',
+      path: '/admin/products',
+      component: require('~/views/admin/Products').default
+    },
+    {
+      name: 'admin.products.create',
+      path: '/admin/products/create',
+      component: require('~/views/admin/Product').default
+    },
+    {
+      name: 'admin.products.edit',
+      path: '/admin/products/:id',
+      component: require('~/views/admin/Product').default
     }
   ]
 });
