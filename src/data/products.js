de9dc59b19297620b0bb6422adeb74abbd22feb0
Charlie Gilman <charliegilmanuk@gmail.com>
2019-02-04 00:46:22 +0000

    Add multiline descriptions

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,15 @@ const products = [
   new Product(
     1,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Voluptate minim aute ex nisi. Reprehenderit incididunt dolor eu voluptate esse in Lorem ullamco proident ullamco. Officia irure eiusmod enim deserunt incididunt nostrud voluptate irure commodo exercitation magna. Consectetur qui aliquip ex nulla in sint exercitation consectetur. Dolor non anim non et voluptate. Nulla mollit ipsum nulla culpa excepteur culpa reprehenderit eiusmod cillum. Non incididunt eiusmod dolore irure sit amet ad veniam.
+
+Irure ea ullamco non ut eiusmod sit est pariatur fugiat commodo sint sunt anim. Eiusmod consectetur fugiat occaecat elit elit nostrud irure minim ex adipisicing. Velit qui magna ullamco aute occaecat aliquip commodo consequat occaecat voluptate sunt deserunt quis. Duis nostrud sunt consequat pariatur eiusmod anim consequat sint do laboris adipisicing do enim. Sunt excepteur ut qui proident laborum labore eu reprehenderit elit sint veniam ad. Labore excepteur eu commodo ex ea adipisicing Lorem. Dolore aute magna non excepteur ut sint laborum.
+
+Lorem nostrud mollit eu sint mollit ea tempor cupidatat dolore laboris sint exercitation consequat. Ex aliqua sunt nisi officia. Aliquip ipsum et veniam pariatur. Minim minim excepteur proident ea occaecat laborum aute commodo adipisicing pariatur pariatur nostrud quis magna. Occaecat minim duis aute adipisicing dolor minim aliquip ex. Est sunt velit non deserunt aliqua id in aute laborum irure voluptate in.
+
+Qui consectetur officia sunt excepteur. Officia occaecat ex duis elit duis veniam aliqua velit exercitation. Nisi fugiat id consequat aliquip reprehenderit nulla elit aliquip anim. Consequat exercitation id cillum adipisicing proident ad. Nulla irure qui ullamco ullamco sint et eu. Dolore consequat quis incididunt in in irure est Lorem ad laborum exercitation consectetur adipisicing id.
+
+Sint veniam excepteur minim ea ex do incididunt nisi ut ad Lorem fugiat. Amet ut in nisi fugiat voluptate excepteur est sint esse. Labore ex est labore cupidatat consequat et est nulla eu tempor cillum tempor aliquip aliquip. Aute aliquip dolore ut quis. Fugiat nostrud adipisicing irure qui laborum qui tempor ullamco adipisicing velit. Sunt dolore ex esse pariatur adipisicing in labore sunt aute do.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-27 17:00:00',
     375,
@@ -13,7 +21,11 @@ const products = [
   new Product(
     2,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Dolor dolor labore duis Lorem amet nisi in culpa minim aute velit aliquip. Aliqua sint sit nisi mollit et ipsum velit tempor veniam dolor. Nostrud aliquip quis deserunt voluptate occaecat magna ipsum pariatur et officia magna labore. Magna veniam occaecat culpa in aliqua enim nulla. Minim enim enim dolor consectetur ad duis ipsum aute aute ex excepteur aute.
+
+Adipisicing sunt qui nisi cupidatat duis dolore nostrud exercitation ea est occaecat labore. Consequat aute occaecat consectetur cillum fugiat officia qui quis officia cupidatat ut commodo. Mollit dolor Lorem mollit quis et elit nisi pariatur sint excepteur culpa ipsum cupidatat. Deserunt eu exercitation voluptate consectetur voluptate. Adipisicing excepteur fugiat tempor dolore mollit laboris labore do cillum. Velit ullamco ullamco quis enim mollit enim. Voluptate laborum laboris deserunt minim dolor velit Lorem.
+
+Id dolore proident occaecat dolor incididunt dolor consectetur mollit fugiat Lorem Lorem. Lorem et nulla id nulla ut anim aute. Labore in sunt cupidatat nostrud duis aliqua voluptate reprehenderit incididunt dolor pariatur enim tempor adipisicing. Consequat cillum dolore esse minim labore veniam pariatur.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-01-14 17:00:00',
     4199,
@@ -22,7 +34,15 @@ const products = [
   new Product(
     3,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Minim quis adipisicing in aute fugiat reprehenderit in dolore voluptate in duis. Magna commodo dolor minim ea duis ex culpa reprehenderit magna laboris magna pariatur. Elit tempor aute ad consequat consectetur laboris mollit cillum reprehenderit esse.
+
+Sit Lorem incididunt consequat do adipisicing nisi. Cupidatat laboris irure ad officia consequat nulla laborum labore aliquip enim irure amet incididunt deserunt. Ut irure mollit mollit excepteur commodo ullamco ea Lorem.
+
+Aliquip cillum ex tempor anim laborum occaecat esse fugiat. Quis Lorem commodo commodo incididunt ad consequat sint voluptate dolore labore. Occaecat excepteur do est non veniam quis in proident nostrud nulla.
+
+Dolore et laboris nisi irure nisi irure in velit. Nisi occaecat esse aliquip elit labore sunt et. Anim veniam exercitation ad cillum. Quis in cillum eiusmod fugiat in veniam aliqua nisi exercitation. Commodo incididunt sunt aliquip nostrud eu ipsum proident adipisicing deserunt qui. Ex minim incididunt amet ullamco nostrud consectetur nisi proident non commodo reprehenderit nisi nisi.
+
+Aliqua eu cillum elit incididunt nulla ut aliqua. Officia aliqua laborum tempor aute culpa aute ea excepteur. Sunt et ut veniam amet.`,
     require('~/assets/products/t-shirt-grey-front.jpg'),
     '2019-02-10 17:00:00',
     1750,
@@ -31,7 +51,13 @@ const products = [
   new Product(
     4,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Qui cillum ullamco irure amet ad. Dolor incididunt elit dolor excepteur. Cupidatat aute laboris non sunt elit Lorem.
+
+Enim occaecat aliquip esse occaecat consectetur magna mollit deserunt occaecat excepteur. Amet nisi ullamco officia eiusmod excepteur pariatur occaecat anim. Consectetur magna exercitation consectetur laborum nulla eu minim cupidatat eu. Ex quis sit occaecat excepteur quis veniam amet. Pariatur adipisicing sit laboris amet ex ullamco esse ullamco incididunt est commodo tempor cupidatat.
+
+Eiusmod do dolore ad aliqua magna ad nulla id. Commodo mollit magna tempor et Lorem dolor tempor officia nisi aute incididunt pariatur laborum. Velit duis eu dolor incididunt minim ea amet exercitation culpa non Lorem laborum nisi. Mollit commodo exercitation fugiat esse. Labore aute velit ex sit incididunt labore officia eiusmod enim.
+
+Laborum aute culpa enim non. Ipsum do culpa dolore ex id laborum id. Ad do laborum et labore nulla aute ex est culpa ex velit. Pariatur non aliqua aliquip sit ea minim officia. Irure sint laboris consequat anim eiusmod sint cupidatat velit nulla enim dolor sint sint sint.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-27 17:00:00',
     288,
@@ -40,7 +66,13 @@ const products = [
   new Product(
     5,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Mollit culpa magna cupidatat cillum aliquip tempor tempor fugiat minim. Dolore excepteur exercitation commodo aliqua exercitation occaecat ea id consectetur minim veniam veniam. Eiusmod minim cillum pariatur dolore nostrud veniam ex id pariatur officia culpa cupidatat. Tempor aute aliqua ut laboris qui. Dolor et cupidatat duis adipisicing mollit aliquip deserunt do cillum.
+
+Incididunt ad adipisicing et quis mollit nulla sunt. Quis ad mollit eu aliqua. Veniam duis nostrud elit nisi nisi duis velit non enim ea pariatur proident elit laboris. Excepteur deserunt ea sint nisi.
+
+Magna deserunt proident reprehenderit ipsum tempor tempor. Minim in velit est id quis pariatur. Cupidatat amet id laboris aliqua adipisicing sunt deserunt consectetur aliquip tempor amet ut.
+
+Nisi ad adipisicing quis laborum qui irure do cupidatat non esse veniam voluptate aliquip elit. Enim aute aute proident aute mollit ea non amet ad. Do eu esse et tempor. Nostrud nisi nulla in nostrud excepteur anim. Commodo id non ipsum amet deserunt pariatur sit. Nisi enim voluptate magna quis velit esse irure eu aliqua culpa.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-02-18 17:00:00',
     1466,
@@ -49,7 +81,17 @@ const products = [
   new Product(
     6,
     'Nexus Mods T-Shirt',
-    `Incididunt eu ipsum voluptate dolore. Adipisicing aliquip elit non cupidatat nostrud eu pariatur mollit dolor reprehenderit amet. Aliqua ullamco deserunt non ex occaecat officia qui aliqua. Minim ullamco ut qui nostrud esse culpa deserunt officia. Ut pariatur consectetur officia est ut reprehenderit incididunt magna adipisicing exercitation id quis quis ullamco. Excepteur qui enim est esse aute minim sit voluptate id. Sint cillum quis irure officia pariatur labore in exercitation laboris sint in consectetur ullamco Lorem.  Ea reprehenderit exercitation magna ea non ea velit veniam cupidatat do. Velit amet sunt duis esse amet Lorem fugiat proident officia reprehenderit ullamco commodo. Velit commodo quis enim in ipsum magna fugiat Lorem labore sit consectetur cupidatat excepteur dolor. Magna Lorem nostrud enim in eu ullamco eiusmod ad. Amet mollit sint velit voluptate sunt.`,
+    `Et non sit labore sint in laborum anim. Excepteur minim eu tempor fugiat duis do laboris qui veniam labore consectetur occaecat cupidatat sit. Aliquip enim ea incididunt occaecat occaecat.
+
+Ullamco cupidatat magna aliquip elit ea minim ex veniam et ex dolore incididunt duis laboris. Quis mollit et eu commodo voluptate et ex aliqua. Do sint nulla tempor adipisicing sunt consequat minim occaecat deserunt nostrud labore eiusmod.
+
+Minim in enim do consequat non dolor. Velit aute quis et sint cillum consequat commodo aliqua ullamco esse ut est magna. Anim mollit ad mollit irure quis excepteur consequat. Fugiat officia fugiat sint qui laborum elit cillum eu deserunt. Deserunt veniam adipisicing nulla nostrud ea cillum voluptate qui sunt aliqua. Dolore nostrud aliquip anim occaecat dolore exercitation.
+
+Adipisicing occaecat reprehenderit cupidatat do officia. Sit in ea consectetur et quis minim cupidatat. Eiusmod laborum deserunt fugiat nostrud ut nulla occaecat aute. Mollit ea aliquip ea irure veniam cupidatat occaecat aliquip nulla elit fugiat do voluptate. Ullamco ipsum nulla do nostrud veniam id sunt reprehenderit magna consectetur sit exercitation do culpa. Aliquip dolor tempor non aliqua ea id.
+
+Irure ex dolor non officia dolor occaecat. Aliqua enim consectetur eu commodo reprehenderit excepteur quis. Cupidatat ea culpa nulla esse voluptate.
+
+Aliqua labore sint aliqua laborum Lorem ad et labore deserunt. Deserunt elit et sunt ea veniam adipisicing. Dolore quis proident ut ut qui nulla et aute mollit consequat culpa adipisicing ea. Ipsum ipsum enim mollit officia aliquip aliquip irure. Esse veniam qui mollit nostrud nisi cillum. Consectetur labore voluptate aute cillum minim et minim tempor qui ex ad sint proident. Enim ut cillum cupidatat magna in esse non.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
     '2019-04-01 17:00:00',
     3780,
