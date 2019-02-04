34ee407687c1118934e6d7d49c579e63778a043f
Charlie Gilman <charliegilmanuk@gmail.com>
2019-02-04 18:26:53 +0000

    Add better variety of mockup images and default data

--- src/data/products.js
+++ src/data/products.js
@@ -3,7 +3,7 @@ import Product from '~/models/Product';
 const products = [
   new Product(
     1,
-    'Nexus Mods T-Shirt',
+    'Branded T-Shirt – Black',
     `Voluptate minim aute ex nisi. Reprehenderit incididunt dolor eu voluptate esse in Lorem ullamco proident ullamco. Officia irure eiusmod enim deserunt incididunt nostrud voluptate irure commodo exercitation magna. Consectetur qui aliquip ex nulla in sint exercitation consectetur. Dolor non anim non et voluptate. Nulla mollit ipsum nulla culpa excepteur culpa reprehenderit eiusmod cillum. Non incididunt eiusmod dolore irure sit amet ad veniam.
 
 Irure ea ullamco non ut eiusmod sit est pariatur fugiat commodo sint sunt anim. Eiusmod consectetur fugiat occaecat elit elit nostrud irure minim ex adipisicing. Velit qui magna ullamco aute occaecat aliquip commodo consequat occaecat voluptate sunt deserunt quis. Duis nostrud sunt consequat pariatur eiusmod anim consequat sint do laboris adipisicing do enim. Sunt excepteur ut qui proident laborum labore eu reprehenderit elit sint veniam ad. Labore excepteur eu commodo ex ea adipisicing Lorem. Dolore aute magna non excepteur ut sint laborum.
@@ -14,28 +14,28 @@ Qui consectetur officia sunt excepteur. Officia occaecat ex duis elit duis venia
 
 Sint veniam excepteur minim ea ex do incididunt nisi ut ad Lorem fugiat. Amet ut in nisi fugiat voluptate excepteur est sint esse. Labore ex est labore cupidatat consequat et est nulla eu tempor cillum tempor aliquip aliquip. Aute aliquip dolore ut quis. Fugiat nostrud adipisicing irure qui laborum qui tempor ullamco adipisicing velit. Sunt dolore ex esse pariatur adipisicing in labore sunt aute do.`,
     require('~/assets/products/t-shirt-black-front.jpg'),
-    '2019-02-27 17:00:00',
+    '2019-03-01 17:00:00',
     '20',
-    375,
-    2000
+    3576,
+    5000
   ),
   new Product(
     2,
-    'Nexus Mods T-Shirt',
+    'Nexus Mods T-Shirt – Grey',
     `Dolor dolor labore duis Lorem amet nisi in culpa minim aute velit aliquip. Aliqua sint sit nisi mollit et ipsum velit tempor veniam dolor. Nostrud aliquip quis deserunt voluptate occaecat magna ipsum pariatur et officia magna labore. Magna veniam occaecat culpa in aliqua enim nulla. Minim enim enim dolor consectetur ad duis ipsum aute aute ex excepteur aute.
 
 Adipisicing sunt qui nisi cupidatat duis dolore nostrud exercitation ea est occaecat labore. Consequat aute occaecat consectetur cillum fugiat officia qui quis officia cupidatat ut commodo. Mollit dolor Lorem mollit quis et elit nisi pariatur sint excepteur culpa ipsum cupidatat. Deserunt eu exercitation voluptate consectetur voluptate. Adipisicing excepteur fugiat tempor dolore mollit laboris labore do cillum. Velit ullamco ullamco quis enim mollit enim. Voluptate laborum laboris deserunt minim dolor velit Lorem.
 
 Id dolore proident occaecat dolor incididunt dolor consectetur mollit fugiat Lorem Lorem. Lorem et nulla id nulla ut anim aute. Labore in sunt cupidatat nostrud duis aliqua voluptate reprehenderit incididunt dolor pariatur enim tempor adipisicing. Consequat cillum dolore esse minim labore veniam pariatur.`,
-    require('~/assets/products/t-shirt-black-front.jpg'),
-    '2019-01-14 17:00:00',
+    require('~/assets/products/t-shirt-grey-front.jpg'),
+    '2019-03-01 17:00:00',
     '20',
-    4199,
-    10000
+    2198,
+    5000
   ),
   new Product(
     3,
-    'Nexus Mods T-Shirt',
+    'Branded Cap – Black',
     `Minim quis adipisicing in aute fugiat reprehenderit in dolore voluptate in duis. Magna commodo dolor minim ea duis ex culpa reprehenderit magna laboris magna pariatur. Elit tempor aute ad consequat consectetur laboris mollit cillum reprehenderit esse.
 
 Sit Lorem incididunt consequat do adipisicing nisi. Cupidatat laboris irure ad officia consequat nulla laborum labore aliquip enim irure amet incididunt deserunt. Ut irure mollit mollit excepteur commodo ullamco ea Lorem.
@@ -45,15 +45,15 @@ Aliquip cillum ex tempor anim laborum occaecat esse fugiat. Quis Lorem commodo c
 Dolore et laboris nisi irure nisi irure in velit. Nisi occaecat esse aliquip elit labore sunt et. Anim veniam exercitation ad cillum. Quis in cillum eiusmod fugiat in veniam aliqua nisi exercitation. Commodo incididunt sunt aliquip nostrud eu ipsum proident adipisicing deserunt qui. Ex minim incididunt amet ullamco nostrud consectetur nisi proident non commodo reprehenderit nisi nisi.
 
 Aliqua eu cillum elit incididunt nulla ut aliqua. Officia aliqua laborum tempor aute culpa aute ea excepteur. Sunt et ut veniam amet.`,
-    require('~/assets/products/t-shirt-grey-front.jpg'),
-    '2019-02-10 17:00:00',
-    '20',
-    1750,
+    require('~/assets/products/cap.jpg'),
+    '2019-02-01 17:00:00',
+    '12',
+    1026,
     1750
   ),
   new Product(
     4,
-    'Nexus Mods T-Shirt',
+    'Branded Drawstring Bag – White',
     `Qui cillum ullamco irure amet ad. Dolor incididunt elit dolor excepteur. Cupidatat aute laboris non sunt elit Lorem.
 
 Enim occaecat aliquip esse occaecat consectetur magna mollit deserunt occaecat excepteur. Amet nisi ullamco officia eiusmod excepteur pariatur occaecat anim. Consectetur magna exercitation consectetur laborum nulla eu minim cupidatat eu. Ex quis sit occaecat excepteur quis veniam amet. Pariatur adipisicing sit laboris amet ex ullamco esse ullamco incididunt est commodo tempor cupidatat.
@@ -61,15 +61,15 @@ Enim occaecat aliquip esse occaecat consectetur magna mollit deserunt occaecat e
 Eiusmod do dolore ad aliqua magna ad nulla id. Commodo mollit magna tempor et Lorem dolor tempor officia nisi aute incididunt pariatur laborum. Velit duis eu dolor incididunt minim ea amet exercitation culpa non Lorem laborum nisi. Mollit commodo exercitation fugiat esse. Labore aute velit ex sit incididunt labore officia eiusmod enim.
 
 Laborum aute culpa enim non. Ipsum do culpa dolore ex id laborum id. Ad do laborum et labore nulla aute ex est culpa ex velit. Pariatur non aliqua aliquip sit ea minim officia. Irure sint laboris consequat anim eiusmod sint cupidatat velit nulla enim dolor sint sint sint.`,
-    require('~/assets/products/t-shirt-black-front.jpg'),
+    require('~/assets/products/drawstring-bag.jpg'),
     '2019-02-27 17:00:00',
-    '20',
+    '15',
     288,
     500
   ),
   new Product(
     5,
-    'Nexus Mods T-Shirt',
+    'Branded Hoodie – Black',
     `Mollit culpa magna cupidatat cillum aliquip tempor tempor fugiat minim. Dolore excepteur exercitation commodo aliqua exercitation occaecat ea id consectetur minim veniam veniam. Eiusmod minim cillum pariatur dolore nostrud veniam ex id pariatur officia culpa cupidatat. Tempor aute aliqua ut laboris qui. Dolor et cupidatat duis adipisicing mollit aliquip deserunt do cillum.
 
 Incididunt ad adipisicing et quis mollit nulla sunt. Quis ad mollit eu aliqua. Veniam duis nostrud elit nisi nisi duis velit non enim ea pariatur proident elit laboris. Excepteur deserunt ea sint nisi.
@@ -77,15 +77,15 @@ Incididunt ad adipisicing et quis mollit nulla sunt. Quis ad mollit eu aliqua. V
 Magna deserunt proident reprehenderit ipsum tempor tempor. Minim in velit est id quis pariatur. Cupidatat amet id laboris aliqua adipisicing sunt deserunt consectetur aliquip tempor amet ut.
 
 Nisi ad adipisicing quis laborum qui irure do cupidatat non esse veniam voluptate aliquip elit. Enim aute aute proident aute mollit ea non amet ad. Do eu esse et tempor. Nostrud nisi nulla in nostrud excepteur anim. Commodo id non ipsum amet deserunt pariatur sit. Nisi enim voluptate magna quis velit esse irure eu aliqua culpa.`,
-    require('~/assets/products/t-shirt-black-front.jpg'),
+    require('~/assets/products/hoodie-black.jpg'),
     '2019-02-18 17:00:00',
-    '20',
+    '30',
     1466,
     1500
   ),
   new Product(
     6,
-    'Nexus Mods T-Shirt',
+    'Branded Mug – White',
     `Et non sit labore sint in laborum anim. Excepteur minim eu tempor fugiat duis do laboris qui veniam labore consectetur occaecat cupidatat sit. Aliquip enim ea incididunt occaecat occaecat.
 
 Ullamco cupidatat magna aliquip elit ea minim ex veniam et ex dolore incididunt duis laboris. Quis mollit et eu commodo voluptate et ex aliqua. Do sint nulla tempor adipisicing sunt consequat minim occaecat deserunt nostrud labore eiusmod.
@@ -97,11 +97,41 @@ Adipisicing occaecat reprehenderit cupidatat do officia. Sit in ea consectetur e
 Irure ex dolor non officia dolor occaecat. Aliqua enim consectetur eu commodo reprehenderit excepteur quis. Cupidatat ea culpa nulla esse voluptate.
 
 Aliqua labore sint aliqua laborum Lorem ad et labore deserunt. Deserunt elit et sunt ea veniam adipisicing. Dolore quis proident ut ut qui nulla et aute mollit consequat culpa adipisicing ea. Ipsum ipsum enim mollit officia aliquip aliquip irure. Esse veniam qui mollit nostrud nisi cillum. Consectetur labore voluptate aute cillum minim et minim tempor qui ex ad sint proident. Enim ut cillum cupidatat magna in esse non.`,
-    require('~/assets/products/t-shirt-black-front.jpg'),
+    require('~/assets/products/mug-white.jpg'),
     '2019-04-01 17:00:00',
-    '20',
+    '10',
     3780,
     15000
+  ),
+  new Product(
+    7,
+    'Branded iPhone Case',
+    `Non commodo dolore proident quis aute veniam incididunt laborum. Reprehenderit ea id laborum occaecat minim ad sunt duis dolor. Mollit enim laboris non ea nostrud.
+
+Nulla aliquip excepteur cillum velit deserunt. Lorem aliqua sint veniam consequat et nisi duis quis est deserunt sit duis minim laboris. Exercitation laboris sint cupidatat pariatur amet ipsum labore aliquip tempor consectetur velit elit. Est anim eu aliquip quis fugiat laborum esse. Ad aliquip dolor nulla dolor dolor commodo.
+
+Esse Lorem ea Lorem id eiusmod duis enim proident. Eu aliquip elit eiusmod occaecat aute dolor commodo voluptate do mollit. Sunt incididunt nostrud excepteur minim. Voluptate non enim consectetur consequat id sunt aliquip eiusmod magna nostrud sit et.`,
+    require('~/assets/products/iphone-case.jpg'),
+    '2019-04-01 17:00:00',
+    '10',
+    126,
+    1000
+  ),
+  new Product(
+    8,
+    'Branded Socks – Black/White',
+    `Fugiat commodo ipsum sint id labore enim magna minim anim mollit incididunt velit. Dolor dolore laboris deserunt veniam Lorem tempor labore incididunt anim laboris. Pariatur minim voluptate nisi est culpa voluptate officia ea voluptate. Esse culpa nostrud exercitation magna sit.
+
+Commodo in quis consequat aliqua sint culpa. Reprehenderit amet laboris quis consequat amet id adipisicing sunt dolore deserunt minim cupidatat dolor. Ea laborum do laboris consectetur sint esse cupidatat qui velit eu dolor amet est velit. Tempor occaecat duis enim ex tempor consectetur veniam. Aute est consectetur consequat ipsum.
+
+Tempor ut est cupidatat in magna in Lorem. Velit esse ea commodo ipsum deserunt non et ea laborum reprehenderit est eu cupidatat. Consequat Lorem labore nulla quis duis do aliqua. Laboris laboris ex deserunt fugiat pariatur veniam mollit. Sit do dolor qui pariatur in id.
+
+Amet anim proident in adipisicing esse anim ea ea duis amet veniam nostrud ullamco. Ullamco non fugiat magna non deserunt excepteur est magna nulla. Fugiat consequat nostrud occaecat consectetur aute sunt id. Velit qui aliquip nulla officia adipisicing ut nostrud ad nostrud ad occaecat incididunt. Eu labore Lorem eiusmod eiusmod pariatur minim nostrud consequat sint irure aute. Magna proident commodo aute non cillum.`,
+    require('~/assets/products/socks.jpg'),
+    '2019-04-01 17:00:00',
+    '8',
+    3780,
+    3000
   )
 ];
 
