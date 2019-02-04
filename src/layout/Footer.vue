c5d5f1bea45756ea5f0397422073add62745319a
Charlie Gilman <charliegilmanuk@gmail.com>
2019-02-04 00:55:40 +0000

    Add basic footer

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <v-toolbar color="dark">
+    <v-toolbar-title>
+      <router-link to="/">
+        <img
+          :src="require('~/assets/logo.png')"
+          alt="Nexus Mods logo"
+          style="width: 120px;"
+        />
+      </router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <p class="pa-3 ma-0 grey--text">
+      Nexus Mods shop concept by Charlie Gilman
+    </p>
+    <v-spacer></v-spacer>
+    <v-btn color="primary">Back to top</v-btn>
+  </v-toolbar>
 </template>
 
 <script>
